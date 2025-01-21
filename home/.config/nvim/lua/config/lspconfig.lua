59501b118f55842c7d58c51188f42cd11e7d14ec
Ferenc Géczi <ferenc.gm@gmail.com>
2025-01-21 00:00:00 +0000

    Remove rendundant debounce setting from lspconfig
    Signed-off-by: Ferenc Géczi <ferenc.gm@gmail.com>

--- home/.config/nvim/lua/config/lspconfig.lua
+++ home/.config/nvim/lua/config/lspconfig.lua
@@ -32,12 +32,3 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 local servers = { 'jedi_language_server', 'rust_analyzer' }
-for _, lsp in pairs(servers) do
-  require('lspconfig')[lsp].setup {
-    on_attach = on_attach,
-    flags = {
-      -- This will be the default in neovim 0.7+
-      debounce_text_changes = 150,
-    }
-  }
-end
